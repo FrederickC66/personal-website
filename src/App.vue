e651e9696f6c0c45423a4f1a1d0a05d5c10cce15
Frederick Cortez <FrederickCortez66@gmail.com>
2023-10-25 22:19:27 -0500

    first html

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
 
 <template>
   <main class="app">
+    <nav class="header">
+      <h3 class="nav-button text"> Home </h3>
+      <h3 class="nav-button text"> About </h3>
+      <h3 class="nav-button text"> Projects </h3>
+      <h3 class="nav-button text"> Contact </h3>
+    </nav>
     <h1 class="text">Hello World</h1>
   </main>
 </template>
